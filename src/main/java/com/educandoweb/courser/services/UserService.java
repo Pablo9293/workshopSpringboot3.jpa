3ec8a822af2473223b9226e35f54d2e045344283
Pablo Cupertino <pabloagm7@gmail.com>
2024-01-26 21:13:20 -0300

    User update

--- src/main/java/com/educandoweb/courser/services/UserService.java
+++ src/main/java/com/educandoweb/courser/services/UserService.java
@@ -35,6 +35,19 @@ public class UserService {
 	public void delete(long id) {
 		repository.deleteById(id);
 	}
+	
+	//atualizar um dado usuario no banco
+	public User update(Long id , User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity , obj);
+		return repository.save(entity);
+	}
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
 
 
